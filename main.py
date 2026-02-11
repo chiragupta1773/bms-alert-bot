e68b7e30a413d9cf6541d44227bff5dd23686998
chiragupta1773 <121121382+chiragupta1773@users.noreply.github.com>
2026-02-11 13:55:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import time
 from bs4 import BeautifulSoup
 
 # ===== CHANGE THESE =====
-EVENT_URL = "https://in.bookmyshow.com/sports/super-8-match-8-icc-men-s-t20-wc-2026/ET00474264"
+EVENT_URLS = [
+    "https://in.bookmyshow.com/sports/super-8-match-8-icc-men-s-t20-wc-2026/ET00474264",
+    "https://in.bookmyshow.com/sports/india-vs-pakistan-icc-men-s-t20-wc-2026/ET00474320"
+]
 TELEGRAM_TOKEN = "8386803926:AAFdoxr8EePx87pz3G8ZJC2snLbWVWYjtjE"
 CHAT_ID = "1349239306"
 CHECK_INTERVAL = 20
@@ -16,29 +19,31 @@ def send_telegram_message(message):
     requests.post(url, data=data)
 
 
-def check_book_now():
-    headers = {"User-Agent": "Mozilla/5.0"}
+3def check_book_now(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-    response = requests.get(EVENT_URL, headers=headers)
+    response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
     if "Book Now" in soup.text:
         return True
     return False
 
-
 print("Bot started...")
 
 while True:
     try:
-        print("Checking page...")
+        for url in EVENT_URLS:
+            print(f"Checking {url} ...")
 
-        if check_book_now():
-            print("BOOK NOW FOUND!")
-            send_telegram_message("🎟 Tickets are LIVE! Go Book Now!")
-            break
-        else:
-            print("Not available yet...")
+            if check_book_now(url):
+                print("BOOK NOW FOUND!")
+                send_telegram_message(f"🎟 Tickets LIVE!\n{url}")
+                break
+            else:
+                print("Not available yet...")
 
     except Exception as e:
         print("Error:", e)
